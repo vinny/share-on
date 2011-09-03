c109c679ae0c650eea609a3c963f6ddd079fd7ee
Marcus Vinny <vinnykun@hotmail.com>
2011-09-03 15:25:11 -0300

    update v2.0.0

--- root/install.php
+++ root/install.php
@@ -86,6 +86,11 @@ $versions = array(
 			),
 		),
    ),
+   
+   	// Version 2.0.0
+	'2.0.0' => array(
+		// Nothing changed in this version.
+	),
 );
 
 // Include the UMIF Auto file and everything else will be handled automatically.
