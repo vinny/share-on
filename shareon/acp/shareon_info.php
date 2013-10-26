8efb23e0a95dd872aacf186b8c6794c0b5893a56
Marcus Vinny <vinny@suportephpbb.com.br>
2013-10-26 03:36:16 -0300

    ACP module

---
+++ shareon/acp/shareon_info.php
@@ -0,0 +1,36 @@
+<?php
+/**
+*
+* @package Share On
+* @copyright (c) 2013 Vinny
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+namespace vinny\shareon\acp;
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* @package module_install
+*/
+class shareon_info
+{
+	function module()
+	{
+		return array(
+			'filename'	=> '\vinny\shareon\acp\shareon_module',
+			'title'		=> 'SO_ACP',
+			'version'	=> '1.1.0',
+			'modes'		=> array(
+				'settings'	=> array('title' => 'SO_CONFIG', 'auth'	=> 'acl_a_board', 'cat'	=> array('SHARE_ON_MOD')),
+			),
+		);
+	}
+}
